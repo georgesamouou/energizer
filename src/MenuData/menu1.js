2649c7456f69528a8ce6130b94855a265afca507
georgesamouou <105161834+georgesamouou@users.noreply.github.com>
2023-07-20 15:51:22 +0200

    final menu

--- src/MenuData/menu1.js
+++ src/MenuData/menu1.js
@@ -1,4 +1,13 @@
 const traitementDoc = {
+        Etats_Paiement:[
+            {title:"Variation",content:["Actifs","Especes","Vires Banques","Vires Cooperatives"]},
+            {title:"Brouillard",content:["Des Actifs","Des Especes","Des vires Microfinances","Des paiement par carte Bancaire","Des expatries","Boordereau Par Employeur","Virement Hepdo Banque","Virement hepdo cooperatives","Des retenues par motifs et par Banque","Des retenues par motif et par cooperative"]},
+            {title:"Recapitulatif",content:["Brouillard des vires","Brouillard des vires hebdo","De definitif des vires hebdo"]},
+            {title:"paiement",content:["Especes par Site/Guichet","Vires par banque","Par cooperative","Vires expatries","Vires hebdo banque","Vires hebdo cooperative"]},
+            {title:"Listing",content:["Des paiements par cartes Bancaire","Paiement icard","Paiement grabadaire","Affichage especes(caisses)","Affichage grabadaire","Des element variable par periode","Des retenues par motifs et par banque","Des retenues par motif et par cooperative"]},
+            {title:"Boordereaux",content:["caisse","Employeur"]}
+
+        ],
         Traitement_des_dossiers:[
 
             {title:"Nombre de dossier",content:["Traiter via la GED-LAD","Par nature prestations","(avec statut)par nature prestation"],
@@ -9,14 +18,6 @@ const traitementDoc = {
             },{title:"Autre",content:["Duree de traitement Dossiers","Recapitulatif Activite Prestations sociales","Depenses Ordonancees en prestations","Ecritures en debits et en creditrs des employeurs","Liste Des dossiers AT/MP","Extraction Listes Des Doissiers AT/MP"]
             }
         ],
-        Etats_Paiement:{
-            title:"Variation",content:["Actifs","Especes","Vires Banques","Vires Cooperatives"],
-            title:"Brouillard",content:["Des Actifs","Des Especes","Des vires Microfinances","Des paiement par carte Bancaire","Des expatries","Boordereau Par Employeur","Virement Hepdo Banque","Virement hepdo cooperatives","Des retenues par motifs et par Banque","Des retenues par motif et par cooperative"],
-            title:"Recapitulatif",content:["Brouillard des vires","Brouillard des vires hebdo","De definitif des vires hebdo"],
-            title:"paiement",content:["Especes par Site/Guichet","Vires par banque","Par cooperative","Vires expatries","Vires hebdo banque","Vires hebdo cooperative"],
-            title:"Listing",content:["Des paiements par cartes Bancaire","Paiement icard","Paiement grabadaire","Affichage especes(caisses)","Affichage grabadaire","Des element variable par periode","Des retenues par motifs et par banque","Des retenues par motif et par cooperative"],
-            title:"Boordereaux",content:["caisse","Employeur"]
-
-        }
+        
 }
 module.exports = traitementDoc;
